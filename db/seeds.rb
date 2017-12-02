b244c8af481cfaaf2b6c8b0ffed589ca642e3bd4
Brian Howald <bdhowald@gmail.com>
2017-12-01 23:35:32 -0500

    Change from sqlite to postgres.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+["pedestrian", "cyclist", "crosswalk", "student", "school", "walking", "biking", "bicycle", "bike", "ride", "complete street", "protected bike lane", "transit", "subway", "train", "bus", "greenway", "bridge", "pathway", "cycling", "child safety", "bronx", "brooklyn", "manhattan", "queens", "staten island", "bike lane", "citywide", "statewide"].each{|word| Tag.create(word: word)}
