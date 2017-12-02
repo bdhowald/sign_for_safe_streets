c58d96b530a4fff04868d6a680247ab4b73da406
Brian Howald <bdhowald@gmail.com>
2017-12-01 22:17:47 -0500

    Adding helper code. Should factor out into a decorator.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,33 @@
 module ApplicationHelper
+
+  def address_complete?(address_obj)
+    if address_obj.present?
+      return (
+        address_obj['street'].present? &&
+        address_obj['city'].present?   &&
+        address_obj['state'].present?  &&
+        address_obj['zip'].present?
+      )
+    end
+
+    return false
+  end
+
   def address_string(address_obj)
-    if address_obj
-      return "#{address_obj['street']}, #{address_obj['city']}, #{address_obj['state']} #{address_obj['zip']}, USA"
+    if address_obj.present?
+      # return "#{address_obj['street']}, #{address_obj['city']}, #{address_obj['state']} #{address_obj['zip']}, USA"
+      if address_obj['state'].present?
+        if address_obj['city'].present?
+          if address_obj['street'].present?
+            "#{address_obj['street']}, #{address_obj['city']}, #{address_obj['state']}, #{address_obj['zip']}, USA"
+          else
+            "#{address_obj['city']}, #{address_obj['state']} #{address_obj['zip']}, USA"
+          end
+        else
+          "#{address_obj['state']} #{address_obj['zip']}, USA"
+        end
+      end
     end
   end
+
 end
--- app/helpers/campaign_helper.rb
+++ app/helpers/campaign_helper.rb
@@ -1,5 +1,49 @@
 module CampaignHelper
 
+
+  def abbreviate_targets(targets)
+    new_str = targets.dup
+
+    if /(bronx|brooklyn|manhattan|queens)/i.match(new_str)
+      new_str.gsub!($&, "#{borough_abbreviation($1)}.")
+    end
+
+    if /staten island/i.match(new_str)
+      new_str.gsub!($&, 'S.I.')
+    end
+
+    new_str.gsub!(/(The )?New York City/, 'NYC')
+    new_str.gsub!(/public advocate/i, 'Pub. Adv.')
+    new_str.gsub!(/Commissioner/, 'Commr.')
+
+    new_str.gsub!(/borough\s*president/i, 'BP')
+    new_str.gsub!(/\bborough\b/i, 'Boro')
+
+    new_str.gsub!(/community\s*board/i, 'CB')
+    new_str.gsub!(/district\s*manager/i, 'DM')
+    new_str.gsub!(/community\s*coordinator/i, 'Comm. Coord.')
+    new_str.gsub!(/community\s*service/i, 'Comm. Serv.')
+
+    new_str.gsub!(/(city )?council\s*(member|man|woman)/i, 'CM')
+
+
+    new_str.gsub!(/assembly\s*(member|man|woman)/i, 'AM')
+    new_str.gsub!(/\bassembly\b/i, 'Ass.')
+    new_str.gsub!(/\bspeaker\b/i, 'Spkr.')
+
+    new_str.gsub!(/\b(NY|New York) State\b/i, 'NYS')
+    new_str.gsub!(/\bsen(ate|ator)?\b/i, 'Sen.')
+    new_str.gsub!(/state\s*senator/i, 'Sen.')
+
+    new_str.gsub!(/governor/i, 'Gov.')
+
+    new_str.gsub!(/Transportation/, 'Transp.')
+    new_str.gsub!(/Committee/, 'Comm.')
+
+    new_str
+  end
+
+
   def borough_abbreviation(borough)
     case borough.downcase
       when "bronx"
@@ -16,17 +60,31 @@ module CampaignHelper
         return "NYC"
       when "statewide"
         return "NYS"
+      else
+        return borough
     end
   end
 
 
-  def target_abbreviation(targets)
-    new_str = targets
-    new_str.gsub!(/borough\s*president/i, 'BP')
-    new_str.gsub!(/community\s*board/i, 'CB')
-    new_str.gsub!(/council\s*(member|man|woman)/i, 'CM')
-    new_str.gsub!(/assembly\s*(member|man|woman)/i, 'AM')
-    new_str.gsub!(/state\s*senator/i, 'SS')
+  def format_letter(letter)
+    new_letter = letter.dup
+    new_letter.gsub!(/\n/, '<br>')
+
+    new_letter.html_safe
+  end
+
+
+  def remove_targets_abbreviations(targets)
+    new_str = targets.dup
+    new_str.gsub!(/\bbp\b/i, 'Borough President')
+    new_str.gsub!(/\bcb\b/i, 'Community Board')
+    new_str.gsub!(/\bcm\b/i, 'Council Member')
+    new_str.gsub!(/\bam\b/i, 'Assembly Member')
+    new_str.gsub!(/\b(ss|sen\.?)\b/i, 'State Senator')
+    new_str.gsub!(/\bspeaker\b/i, 'Assemby Speaker')
+
+    new_str.gsub!(/\bNYC\b/i, 'New York City')
+    new_str.gsub!(/\bDOT\b/i, 'Department of Transportation')
     new_str
   end
 
