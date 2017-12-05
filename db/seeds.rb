d60972c39cefb379bd504a08c1dc9bbe2ae98be0
Brian Howald <bdhowald@gmail.com>
2017-12-05 17:57:51 -0500

    Updated seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,113 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-["pedestrian", "cyclist", "crosswalk", "student", "school", "walking", "biking", "bicycle", "bike", "ride", "complete street", "protected bike lane", "transit", "subway", "train", "bus", "greenway", "bridge", "pathway", "cycling", "child safety", "bronx", "brooklyn", "manhattan", "queens", "staten island", "bike lane", "citywide", "statewide"].each{|word| Tag.find_or_create_by(word: word)}
+[
+  "pedestrian",
+  "cyclist",
+  "crosswalk",
+  "student",
+  "school",
+  "walking",
+  "biking",
+  "bicycle",
+  "bike",
+  "ride",
+  "complete street",
+  "protected bike lane",
+  "transit",
+  "subway",
+  "train",
+  "bus",
+  "greenway",
+  "bridge",
+  "pathway",
+  "cycling",
+  "child safety",
+  "bronx",
+  "brooklyn",
+  "manhattan",
+  "queens",
+  "staten island",
+  "bike lane",
+  "citywide",
+  "statewide"
+].each{|word|
+  Tag.find_or_create_by(word: word)
+}
+
+[
+  'cycling',
+  'walking',
+  'transit',
+  'schools'
+].each{|word|
+  Category.find_or_create_by(name: word)
+}
+
+cycling = Category.find_by(name: 'cycling')
+[
+  "cyclist",
+  "biking",
+  "bike",
+  "ride",
+  "complete street",
+  "protected bike lane",
+  "greenway",
+  "bridge",
+  "pathway",
+  "cycling",
+  "bike lane",
+].each{|word|
+  tag = Tag.find_by(word: word)
+  if !cycling.tags.include?(tag)
+    cycling.tags << tag
+  end
+}
+
+walking = Category.find_by(name: 'walking')
+[
+  "pedestrian",
+  "crosswalk",
+  "walking",
+  "complete street",
+  "greenway",
+  "bridge",
+  "pathway"
+].each{|word|
+  tag = Tag.find_by(word: word)
+  if !walking.tags.include?(tag)
+    walking.tags << tag
+  end
+}
+
+transit = Category.find_by(name: 'transit')
+[
+  "transit",
+  "subway",
+  "train",
+  "bus"
+].each{|word|
+  tag = Tag.find_by(word: word)
+  if !transit.tags.include?(tag)
+    transit.tags << tag
+  end
+}
+
+schools = Category.find_by(name: 'schools')
+[
+  "student",
+  "school",
+  "child safety"
+].each{|word|
+  tag = Tag.find_by(word: word)
+  if !schools.tags.include?(tag)
+    schools.tags << tag
+  end
+}
+
+
+
+
+
+
+
