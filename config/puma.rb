896b0fbbf9aa2359f1e0b867902656f2170c877d
Brian Howald <bdhowald@gmail.com>
2017-12-29 00:18:32 +0000

    Removed unnecessary application config for puma.rb

--- config/puma.rb
+++ config/puma.rb
@@ -1,17 +1,9 @@
-require 'erb'
-ActiveRecord::Base.establish_connection( YAML.load( ERB.new( File.read( "#{app_dir}/config/database.yml" )).result)[rails_env])
 # Puma can serve each request in a thread from an internal thread pool.
 # The `threads` method setting takes two numbers: a minimum and maximum.
 # Any libraries that use thread pools should be configured to match
 # the maximum value specified for Puma. Default is set to 5 threads for minimum
 # and maximum; this matches the default thread size of Active Record.
 #
-threads_count = ENV.fetch("RAILS_MAX_THREADS").to_i { 5 }
-threads threads_count, threads_count
-
-# Specifies the `port` that Puma will listen on to receive requests; default is 3000.
-#
-port        ENV.fetch("PORT") { 3000 }
 
 # Specifies the `environment` that Puma will run in.
 #
@@ -70,6 +62,19 @@ shared_dir = "#{app_dir}/shared"
 # Default to production
 rails_env = ENV['RAILS_ENV'] || "production"
 environment rails_env
+
+max_threads = begin
+  ENV.fetch("RAILS_MAX_THREADS").to_i { 5 }
+rescue KeyError => e
+  5
+end
+
+threads_count = max_threads
+threads threads_count, threads_count
+
+# Specifies the `port` that Puma will listen on to receive requests; default is 3000.
+#
+port        ENV.fetch("PORT") { 3000 }
 
 # Set up socket location
 bind "unix://#{shared_dir}/sockets/puma.sock"
