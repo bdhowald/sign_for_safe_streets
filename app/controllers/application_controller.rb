64df05a05c0bf7db636003b4d82a0f651fc9405d
Brian Howald <bdhowald@gmail.com>
2018-01-05 14:56:53 -0500

    Back-forward cache doesn't reflect accurate state. Don't cache it as the campaigns are cached (Russian-Doll Caching).

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -29,6 +29,11 @@ class ApplicationController < ActionController::Base
 
     @other_campaigns  = Campaign.active.where.not(id: @campaigns.collect(&:id))
 
+
+    response.headers["Cache-Control"] = "no-cache, no-store, max-age=0, must-revalidate"
+    response.headers["Pragma"] = "no-cache"
+    response.headers["Expires"] = "Fri, 01 Jan 1990 00:00:00 GMT"
+
     render
   end
 
